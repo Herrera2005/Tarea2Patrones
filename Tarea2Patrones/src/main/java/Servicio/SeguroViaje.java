25962e820ba7cb97c736332f682ddf8c4ce3c71f
Engrosal <151480848+Engrosal@users.noreply.github.com>
2025-01-17 22:48:41 -0500

    Parte del Decorator

--- Tarea2Patrones/src/main/java/Servicio/SeguroViaje.java
+++ Tarea2Patrones/src/main/java/Servicio/SeguroViaje.java
@@ -2,4 +2,12 @@ package Servicio;
 
 public class SeguroViaje extends Servicio {
 
+    public SeguroViaje(int costo) {
+        super(costo);
+    }
+
+    @Override
+    public String descripcion() {
+        return "Seguro de Viaje";
+    }
 }
--- Tarea2Patrones/src/main/java/Servicio/Servicio.java
+++ Tarea2Patrones/src/main/java/Servicio/Servicio.java
@@ -3,4 +3,17 @@ package Servicio;
 public abstract class Servicio {
     private int costo;
     
-} 
+    public Servicio(int costo) {
+        this.costo = costo;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public abstract String descripcion();
+}
