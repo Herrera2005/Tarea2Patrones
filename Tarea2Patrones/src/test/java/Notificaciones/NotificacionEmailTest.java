24b91a2d36127980373b0ca77eac94f6014e1d0c
Herrera2005 <criaherr@espol.edu.ec>
2025-01-30 21:05:47 -0500

    modificacion test notificaciones

--- Tarea2Patrones/src/test/java/Notificaciones/NotificacionEmailTest.java
+++ Tarea2Patrones/src/test/java/Notificaciones/NotificacionEmailTest.java
@@ -101,4 +101,41 @@ class NotificacionEmailTest {
         notificacion.setCorreo("nuevo@example.com");
         assertEquals("nuevo@example.com", notificacion.getCorreo());
     }
+    
+    /*Escenario de falla 1: Correo vacío*/
+    // Identificador: testNotificarEmailConCorreoVacio
+    // Propósito: Verificar que la notificación falle si el correo es vacío.
+    // Precondiciones: Cliente y correo vacío.
+    // Entradas: "Mensaje de prueba", cliente con nombre "Juan Pérez", correo vacío.
+    // Salidas Esperadas: Se espera que lance una excepción debido a que el correo está vacío.
+
+    @Test
+    public void testNotificarEmailConCorreoVacio() {
+        Cliente cliente = new Cliente(123456, "Juan Pérez", "juan@email.com", "password");
+        NotificacionEmail notificacion = new NotificacionEmail("");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            notificacion.notificar("Mensaje de prueba", cliente);
+        });
+
+        assertEquals("El correo no puede estar vacío", exception.getMessage());
+    }
+    
+//Escenario de falla 2: Cliente nulo
+    // Identificador: testNotificarEmailConClienteNulo
+    // Propósito: Verificar que la notificación falle si el cliente es nulo.
+    // Precondiciones: Cliente nulo.
+    // Entradas: "Mensaje de prueba", cliente nulo.
+    // Salidas Esperadas: Se espera que lance una excepción debido a que el cliente es nulo.
+
+    @Test
+    public void testNotificarEmailConClienteNulo() {
+        NotificacionEmail notificacion = new NotificacionEmail("juan@email.com");
+
+        Exception exception = assertThrows(NullPointerException.class, () -> {
+            notificacion.notificar("Mensaje de prueba", null);
+        });
+
+        assertEquals("El cliente no puede ser nulo", exception.getMessage());
+    }
 }
